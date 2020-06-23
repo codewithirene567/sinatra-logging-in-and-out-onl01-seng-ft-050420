5db558a747409cf8e201698dca5f02a8f005353e
Irene Scott <irenejinscott@gmail.com>
2020-06-23 01:20:16 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,12 +1,12 @@
 class Helpers
 
-  def self.current_user(@user.id)
+  def self.current_user(session)
     user = User.find_by(session[:user_id])
     user
   end
 
-  def self.is_logged_in?(@user.id)
-    if session[:user_id] = @user.id
+  def self.is_logged_in?(session)
+    !!session[:user_id]
       true
     else
       false
