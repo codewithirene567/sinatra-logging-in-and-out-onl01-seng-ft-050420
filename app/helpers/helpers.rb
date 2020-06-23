06bd6ab01e679a2add02cc0b3907e3802a0ebd1d
Irene Scott <irenejinscott@gmail.com>
2020-06-23 01:12:40 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,8 @@
 class Helpers
 
   def self.current_user(session[:user_id])
-    User.find_by(session[:user_id])
-    
+    user = User.find_by(session[:user_id])
+    user
   end
 
   def self.is_logged_in?
