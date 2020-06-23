cfcb8768885b94fb32700647437ab13c9d8bc9ff
Irene Scott <irenejinscott@gmail.com>
2020-06-23 01:23:04 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,12 +1,12 @@
 class Helpers
 
-  def self.current_user(session)
+  def self.current_user(session)#this is taking in an argument of a session hash
     @user = User.find_by_id(session[:user_id])
   end
 
   def self.is_logged_in?(session)
     !!session[:user_id]
-  
+  #
   end
 
 end
